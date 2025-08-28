d86109ab355f2f102198fb0c027e742e45856de2
Harald Hamrin Reinhed <haraldhr@kth.se>
2025-08-28 13:47:42 +0200

    purposeful success

--- test_function.py
+++ test_function.py
@@ -3,7 +3,7 @@ from function import add
 
 class TestAddFunction(unittest.TestCase):
     def test_positive_numbers(self):
-        self.assertEqual(add(2, 3), 4)
+        self.assertEqual(add(2, 3), 5)
 
     def test_negative_numbers(self):
         self.assertEqual(add(-2, -3), -5)
